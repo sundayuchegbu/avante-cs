287eea575df114ac327cec6763bf8be54d0fff8e
sundayuchegbu <sundayuchegbu10@gmail.com>
2023-03-15 09:38:05 +0100

    fourth commit

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,87 +1,99 @@
 import styles, { layout } from "../style";
 import vector from "../images/vector.png";
+import vector4 from "../images/vector4.jpg";
 
 import React from "react";
 
 const Card = () => (
-  <div class="grid grid-cols-2 gap-4">
-    <div class="mx-16 rounded-lg shadow-lg">
-      <div class="flex mr-24 ">
-        <div class="flex w-1/3  mr-8 border-none bg-primary rounded-[50%] h-[60px] w-[60px] mt-16 border-r border-gray-200">
-          <img
-            src={vector}
-            alt="Image"
-            class="mt-2 mr-8  absolute insert-0 float-left object-cover"
-          />
-        </div>
-        <div class="p-4 w-2/3">
-          <h2 class="text-xl font-bold mb-2">Card Title</h2>
-          <p class="text-gray-700 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            posuere, nunc vel fringilla pharetra, nisl velit sagittis lacus, in
-            pellentesque sapien nisi eu velit.
-          </p>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
+  <div className="relative ">
+    <div className="flex flex-col-1 justify-end items-end mb-8">
+      <h1 className=" mb-4">jsjsjdjkdkdk</h1>
+      <p className="">jdjdjdjdj</p>
+    </div>
+    <div className="grid grid-cols-2 gap-0 ">
+      <div className=" mx-16 rounded-lg shadow-lg bg-card mb-8">
+        <div className="flex mr-24 -ml-8 z-0   ">
+          <div className="flex w-1/3 mr-8  bg-primary rounded-[50%] h-[60px] w-[60px] mt-16 border-r">
+            <img
+              src={vector}
+              alt="Image"
+              className="mt-2 absolute insert-0 object-cover"
+            />
+          </div>
+          <div className="p-4 w-2/3">
+            <h2 className="text-xl font-bold max-w-[470px] text-white mb-4  ">
+              Business Consulting
+            </h2>
+            <p className="text-white max-w-[470px] mb-4   ">
+              Lorem ipsum dolor sit amet consectetur. Proin vel orci temporvelit
+              mattis tempor at nisiLorem ipsum dolor sit amet consectetur. Proin
+              vel orci tempor velit mattis tempor at nisi.
+            </p>
+            <button className=" text-white font-bold py-2 px-4 rounded">
+              Read more
+            </button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="bg-white rounded-lg shadow-lg">
-      <div class="flex">
-        <div class="p-4 w-2/3">
-          <h2 class="text-xl font-bold mb-2">Card Title</h2>
-          <p class="text-gray-700 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            posuere, nunc vel fringilla pharetra, nisl velit sagittis lacus, in
-            pellentesque sapien nisi eu velit.
-          </p>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
-        </div>
-        <div class="w-1/3 border-l border-gray-200">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Image"
-            class="w-full h-full object-cover"
-          />
+      <div className="  mx-16 rounded-lg shadow-lg bg-card mt-12 ">
+        <div className="flex ml-24 -mr-8 z-0 ">
+          <div className="p-4 w-2/3">
+            <h2 className="text-xl text-white font-bold mb-2">Card Title 2</h2>
+            <p className="text-white max-w-[470px] mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              posuere, nunc vel fringilla pharetra, nisl velit sagittis lacus,
+              in pellentesque sapien nisi eu velit.
+            </p>
+            <button className=" text-white font-bold py-2 px-4 rounded">
+              Read more
+            </button>
+          </div>
+          <div className="flex w-1/3 ml-24  bg-primary rounded-[50%] h-[60px] w-[60px] mt-16 border-r ">
+            <img src={``} alt="Image" className="mt-2 absolute  object-cover" />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="bg-white rounded-lg shadow-lg">
-      <div class="flex">
-        <div class="w-1/3 border-r border-gray-200">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Image"
-            class="w-full h-full object-cover"
-          />
-        </div>
-        <div class="p-4 w-2/3">
-          <h2 class="text-xl font-bold mb-2">Card Title</h2>
-          <p class="text-gray-700 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            posuere, nunc vel fringilla pharetra, nisl velit sagittis lacus, in
-            pellentesque sapien nisi eu velit.
-          </p>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
+      <div className=" mx-16 rounded-lg shadow-lg bg-card mb-8 ">
+        <div className="flex  mr-24 -ml-8 z-0">
+          <div className=" flex w-1/3 mr-8  bg-primary rounded-[50%] h-[60px] w-[60px] mt-16 border-l ">
+            <img
+              src={``}
+              alt="Image"
+              className="mt-2 absolute insert-0 object-cover"
+            />
+          </div>
+          <div className="p-4 w-2/3 ">
+            <h2 className="text-xl text-white font-bold mb-2">Card Title3</h2>
+            <p className="text-white max-w-[470px] mb-4 ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              posuere, nunc vel fringilla pharetra, nisl velit sagittis lacus,
+              in pellentesque sapien nisi eu velit.
+            </p>
+            <button className=" text-white font-bold py-2 px-4 rounded">
+              Read more
+            </button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="bg-white rounded-lg shadow-lg">
-      <div class="flex">
-        <div class="p-4 w-2/3">
-          <h2 class="text-xl font-bold mb-2">Card Title</h2>
-          <p class="text-gray-700 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            posuere, nunc vel fringilla pharetra,
-          </p>
+      <div className="  mx-16 rounded-lg shadow-lg bg-card mt-12 ">
+        <div className="flex ml-24 -mr-8 z-0 ">
+          <div className="p-4 w-2/3">
+            <h2 className="text-xl text-white font-bold mb-2">Card Title 2</h2>
+            <p className="text-white max-w-[470px] mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              posuere, nunc vel fringilla pharetra, nisl velit sagittis lacus,
+              in pellentesque sapien nisi eu velit.
+            </p>
+            <button className=" text-white font-bold py-2 px-4 rounded">
+              Read more
+            </button>
+          </div>
+          <div className="flex w-1/3 ml-24  bg-primary rounded-[50%] h-[60px] w-[60px] mt-16 border-r ">
+            <img src={``} alt="Image" className="mt-2 absolute  object-cover" />
+          </div>
         </div>
       </div>
     </div>
